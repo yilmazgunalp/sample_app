b68df4da60e98ae0e22dde16615867f937f3661b
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2017-04-22 22:15:55 +1000

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -73,4 +73,9 @@ test "email should be present" do
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+
+
 end
