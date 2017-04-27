d29995f8ccf41d753721006b123a2d84e1390439
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2017-04-27 20:38:23 +1000

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ validates :email, 	presence: true, length: { maximum: 255 }, format: { with: VAL
 					uniqueness: { case_sensitive: false }
 
 has_secure_password
-validates :password, presence: true, length: { minimum: 6 }
+validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
  # Returns the hash digest of the given string.
   def User.digest(string)
