f6f9a27808d22e76933fc1ef5e9829ed16bcb7f3
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2017-06-09 18:32:20 +1000

    added S3_region with aws prefix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,6 +5,8 @@ if Rails.env.production?
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :aws_s3_region => ENV['S3_REGION']
+
       
     }
     config.fog_directory     =  ENV['S3_BUCKET']
